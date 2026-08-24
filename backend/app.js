bde36a2a904fc67ba3fb02b2545b43d15536eb65
--local <--local>
2026-08-25 02:12:49 +0530

    Resolve bug

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,15 @@ const userRouter = require("./routers/user.route.js");
 const TodoRouter = require("./routers/todo.route.js");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: "http://192.168.49.2:30532",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 app.use(body_parser.json());
 
 app.use('/',userRouter);
